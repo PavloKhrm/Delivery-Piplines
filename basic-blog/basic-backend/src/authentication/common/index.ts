25754d2a3eb4ad47d051c72315d6d1801ae112c5
DerakhshanRadbareh <146732331+DerakhshanRadbareh@users.noreply.github.com>
2025-10-20 19:35:02 +0200

    Added mock password 'demo_password'
    Updated user passwords to 'demo_password' for all entries.

--- basic-blog/basic-backend/src/authentication/common/index.ts
+++ basic-blog/basic-backend/src/authentication/common/index.ts
@@ -1,12 +1,12 @@
 import User from '@/authentication/types/user';
 
 export const users: User[] = [
-  { id: 1, username: 'john', password: 'doe' },
-  { id: 2, username: 'test', password: '123' },
-  { id: 3, username: 'emit-it', password: 'emit' },
-  { id: 4, username: 'admin', password: 'admin' },
-  { id: 5, username: 'user', password: 'user' },
-  { id: 6, username: 'skibidi', password: 'bop' },
-  { id: 7, username: 'hacker', password: 'unhackable' },
-  { id: 8, username: 'nhl', password: 'stenden' },
+  { id: 1, username: 'john', password: 'demo_password' },
+  { id: 2, username: 'test', password: 'demo_password' },
+  { id: 3, username: 'emit-it', password: 'demo_password' },
+  { id: 4, username: 'admin', password: 'demo_password' },
+  { id: 5, username: 'user', password: 'demo_password' },
+  { id: 6, username: 'skibidi', password: 'demo_password' },
+  { id: 7, username: 'hacker', password: 'demo_password' },
+  { id: 8, username: 'nhl', password: 'demo_password' },
 ];
